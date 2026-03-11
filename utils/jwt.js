898b696bfab5f18b34d0d4334ea5e039eecd64c9
SncOne <turkergurel19@gmail.com>
2026-03-12 00:05:13 +0300

    tokenfix

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const crypto = require('crypto');
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '7d';
@@ -39,7 +40,8 @@ const generateTokenPair = (user) => {
     id: user.id,
     email: user.email,
     isGuest: user.is_guest,
-    authProvider: user.auth_provider
+    authProvider: user.auth_provider,
+    jti: crypto.randomBytes(16).toString('hex')
   };
 
   return {
